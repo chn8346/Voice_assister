ca7098e0fdca4526f6b36df01ed5d41bd5627f10
fanch <fanch_163.com>
2020-05-08 12:19:21 +0800

    Push First

--- app/src/main/java/com/example/phoneui/assistant.java
+++ app/src/main/java/com/example/phoneui/assistant.java
@@ -19,11 +19,11 @@ public class assistant {
 
         if(is_init_utility)
         {
-            toast.show(context, "init utility fail", 2000);
+            toast.show(context, "init utility success", 1000);
         }
         else
         {
-            toast.show(context, "init utility success", 2000 );
+            toast.show(context, "init utility fail", 1000 );
         }
     }
     public String assistant_main()
@@ -49,6 +49,28 @@ public class assistant {
         if(mIat == null)
         {
             toast.show(context_, "init recognizer fail", toast.short_time_len);
+        }
+        else
+        {
+            //设置语法ID和 SUBJECT 为空，以免因之前有语法调用而设置了此参数；或直接清空所有参数，具体可参考 DEMO 的示例。
+            mIat.setParameter( SpeechConstant.CLOUD_GRAMMAR, null );
+            mIat.setParameter( SpeechConstant.SUBJECT, null );
+            //设置返回结果格式，目前支持json,xml以及plain 三种格式，其中plain为纯听写文本内容
+            mIat.setParameter(SpeechConstant.RESULT_TYPE, "json");
+            //此处engineType为“cloud”
+            mIat.setParameter( SpeechConstant.ENGINE_TYPE, "cloud");
+            //设置语音输入语言，zh_cn为简体中文
+            mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+            //设置结果返回语言
+            mIat.setParameter(SpeechConstant.ACCENT, "mandarin");
+            // 设置语音前端点:静音超时时间，单位ms，即用户多长时间不说话则当做超时处理
+            //取值范围{1000～10000}
+            mIat.setParameter(SpeechConstant.VAD_BOS, "4000");
+            //设置语音后端点:后端点静音检测时间，单位ms，即用户停止说话多长时间内即认为不再输入，
+            //自动停止录音，范围{0~10000}
+            mIat.setParameter(SpeechConstant.VAD_EOS, "1000");
+            //设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
+            mIat.setParameter(SpeechConstant.ASR_PTT,"1");
         }
     }
 
