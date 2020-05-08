2d0d24d32decdf93c4e065c0a827a0d54c239508
fanch <fanch_163.com>
2020-05-08 17:40:12 +0800

    Push First

---
+++ app/src/main/java/com/example/phoneui/inits.java
@@ -0,0 +1,14 @@
+package com.example.phoneui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class inits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inits);
+    }
+}
