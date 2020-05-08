fe465d9b016040d416046b5002c0bf3678f9076a
fanch <fanch_163.com>
2020-05-08 20:53:33 +0800

    Push First

--- app/src/main/java/com/example/phoneui/file_writer.java
+++ app/src/main/java/com/example/phoneui/file_writer.java
@@ -11,6 +11,12 @@ public class file_writer {
     private SharedPreferences reader = context_.getSharedPreferences(fileName, Context.MODE_PRIVATE);
     private SharedPreferences.Editor writer = reader.edit();
 
+    boolean init_ok = true;
+
+    public void file_writer()
+    {
+        change_file(fileName);
+    }
 
     public void change_file(String filename)
     {
@@ -18,38 +24,67 @@ public class file_writer {
         reader = context_.getSharedPreferences(fileName, Context.MODE_PRIVATE);
         writer = reader.edit();
         writer.apply();
+
+        if(reader == null)
+        {
+            init_ok = false;
+        }
     }
 
     public void write(String key, String value)
     {
+        if(!init_ok)
+        {
+            return;
+        }
         writer.putString(key, value);
         writer.apply();
     }
 
     public void write(String key, int value)
     {
+        if(!init_ok)
+        {
+            return;
+        }
         writer.putInt(key, value);
         writer.apply();
     }
 
     public void write(String key, boolean value)
     {
+        if(!init_ok)
+        {
+            return;
+        }
         writer.putBoolean(key, value);
         writer.apply();
     }
 
     public String read(String key, String v)
     {
+        if(!init_ok)
+        {
+            return "";
+        }
         return reader.getString(key, "");
     }
 
     public int read(String key, int v)
     {
+        if(!init_ok)
+        {
+            return -1;
+        }
         return reader.getInt(key, 0);
     }
 
     public boolean read(String key, boolean v)
     {
+        if(!init_ok)
+        {
+            return false;
+        }
         return reader.getBoolean(key, false);
     }
 
