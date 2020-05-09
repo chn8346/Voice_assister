28263a9fa3d55ef6441b2211b7cd3f6b525380eb
fanch <fanch_163.com>
2020-05-09 16:47:25 +0800

    1.speaker modify 2.init modify 3.2rd init select

---
+++ app/src/main/java/com/example/phoneui/init2.java
@@ -0,0 +1,14 @@
+package com.example.phoneui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class init2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init2);
+    }
+}
