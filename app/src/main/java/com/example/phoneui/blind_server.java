03f5ce5f9690ed7ac4763b49671b939753aa2a9b
fanch <fanch_163.com>
2020-05-15 16:52:21 +0800

    盲人服务全局层升级

--- app/src/main/java/com/example/phoneui/blind_server.java
+++ app/src/main/java/com/example/phoneui/blind_server.java
@@ -1,28 +1,91 @@
 package com.example.phoneui;
 
 import android.accessibilityservice.AccessibilityService;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.os.Build;
+import android.util.Log;
+import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
+import android.widget.Button;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
 
 /**
  * This class demonstrates how an accessibility service can query
  * window content to improve the feedback given to the user.
  */
+
 public class blind_server extends AccessibilityService{
+
+    // 悬浮窗的控件对象和参数
+    private boolean init_window_manager = false;
+    private WindowManager windowManager;
+    private WindowManager.LayoutParams layoutParams;
+    private Button button;
+
 
     @Override
     protected void onServiceConnected() {
         super.onServiceConnected();
 
+        if(!init_window_manager) {
+            init_window_manager = true;
+
+            windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+            // 新建悬浮窗控件
+            button = new Button(getApplicationContext());
+            button.setText("Floating Window");
+            button.setBackgroundColor(Color.BLUE);
+
+            // 设置LayoutParam
+            layoutParams = new WindowManager.LayoutParams();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                layoutParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+            } else {
+                layoutParams.type = WindowManager.LayoutParams.TYPE_PHONE;
+            }
+            layoutParams.format = PixelFormat.RGBA_8888;
+            layoutParams.width = 500;
+            layoutParams.height = 100;
+            layoutParams.x = 300;
+            layoutParams.y = 300;
+
+            // 将悬浮窗控件添加到WindowManager
+            //windowManager.addView(button, layoutParams);
+        }
     }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-
+        JSONObject json = new JSONObject();
+        json.put("type", event.getEventType());
+        json.put("package", event.getPackageName().toString());
+        Log.d("__Event__", json.toJSONString());
     }
 
     @Override
     public void onInterrupt() {
 
     }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        init_window_manager = false;
+        //windowManager.removeView(button);
+    }
+
+
+
+
 }
